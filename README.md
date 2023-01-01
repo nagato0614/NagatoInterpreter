dd1d4ac6b66b85df18c0bb222814bf32520fe62c
nagato0614 <nagato0614@gmail.com>
2023-01-01 22:03:37 +0900

    update

---
+++ README.md
@@ -0,0 +1,33 @@
+# 自作プログラミング言語実行環境
+Rust 勉強のため自作言語用のインタプリタを作成する
+
+## 自作プログラムの使用 
+
+```
+    1 + 1
+    a = 1
+    b = 2
+    c = a + b
+    c
+```
+
+型はすべて32bit int とする.
+一行ごとに解析する.
+変数だけを記述すると, 標準出力に値を表示する.
+四則演算に対応するが, 0除算は怒らないものとする
+同じ変数が使われたときは値が更新される
+スペースで意味ごとに区切る
+変数への格納でなければ結果を出力する
+
+## Backus Naur Form
+- Equation : 式
+- Variable : 変数
+- Value : 値
+- Arithmetic Equation (AE) : 算術式 
+- Arithmetic operand (AO) : 算術演算オペランド (+, -, *, /, %)
+```
+  Equation := Value | Variable | AE
+  AE := (Value | Variable) AO (Value | Variable)
+  AO := (+|-|*|/|%)
+  Variable := (a-z)+
+```
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,84 @@
+use MylangExecuter::executer::NagatoLang;
+use MylangExecuter::tree::TreeNode;
 use clap::Parser;
 use std::fs::File;
 use std::io::{BufRead, BufReader, stdin};
-use anyhow::{bail, ensure, Context, Result};
 use std::path::PathBuf;
+use anyhow::{bail, ensure, Context, Result};
 
-
-#[derive(Parser, Debug)]
+#[derive(Parser, Debug, Default)]
 #[clap(
 name = "My Lang Execute",
 version = "0.0.1",
 author = "nagai toru",
 about = "my lang parser"
 )]
-struct Program
+pub struct InputParser
 {
     #[clap(short, long)]
-    verbose: bool,
+    pub debug: bool,
 
-    #[clap(name = "FILE")]
-    formula_file: Option<PathBuf>,
+    #[clap(name = "program.txt")]
+    pub formula_file: Option<PathBuf>,
 }
 
+
 fn main() {
-    println!("Hello, world!");
+    let parser = InputParser::parse();
+    let mut exec = NagatoLang::new();
+    if let Some(path) = parser.formula_file
+    {
+        println!("file path : {:?}", path);
+        let f = File::open(path).unwrap();
+        let reader = BufReader::new(f);
+        match exec.run(reader)
+        {
+            Ok(_) => println!("Finish Program"),
+            Err(e) => println!("Error Program : {:?}", e),
+        }
+
+    }
+    else
+    {
+        println!("can't find file");
+    }
+}
+
+
+#[cfg(test)]
+mod tests
+{
+    use std::borrow::Borrow;
+    use std::rc::Rc;
+    use MylangExecuter::tree::{create_node, show_tree};
+    use super::*;
+
+    // 変数の格納取り出し用テスト
+    #[test]
+    fn variable_test()
+    {
+        let mut exec = NagatoLang::new();
+        exec.add_variable("a".to_string(), 1);
+        exec.add_variable("b".to_string(), 2);
+        assert_eq!(exec.get_variable_value("a".to_string()).unwrap(), 1);
+        assert_eq!(exec.get_variable_value("b".to_string()).unwrap(), 2);
+
+        exec.add_variable("a".to_string(), 5);
+        assert_eq!(exec.get_variable_value("a".to_string()).unwrap(), 5);
+    }
+
+    #[test]
+    fn parse_test()
+    {
+
+    }
+
+    #[test]
+    fn tree_test()
+    {
+        let mut root = create_node("1".to_string());
+        root.borrow_mut().add_right_node(TreeNode::new("2".to_string()));
+        root.borrow_mut().add_left_node(TreeNode::new("3".to_string()));
+        println!("show");
+    }
 }
